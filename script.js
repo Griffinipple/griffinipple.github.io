208b542fef1fe0aae0f0651cc8c4aad49030a376
Skibidibble <168710375+Griffinipple@users.noreply.github.com>
2025-01-25 19:48:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -40,6 +40,67 @@ function init() {
     scene.add(platform);
     objects.push(platform);
 
+    // Create ramps
+    const rampGeometry = new THREE.BoxGeometry(20, 1, 14.14); // 14.14 is approximately 20/√2
+    const rampMaterial = new THREE.MeshPhongMaterial({ color: 0x707070 });
+
+    // North ramp
+    const northRamp = new THREE.Mesh(rampGeometry, rampMaterial);
+    northRamp.position.set(0, 6, -17);
+    northRamp.rotation.x = Math.PI / 4; // 45 degrees
+    scene.add(northRamp);
+    objects.push(northRamp);
+
+    // South ramp
+    const southRamp = new THREE.Mesh(rampGeometry, rampMaterial);
+    southRamp.position.set(0, 6, 17);
+    southRamp.rotation.x = -Math.PI / 4;
+    scene.add(southRamp);
+    objects.push(southRamp);
+
+    // East ramp
+    const eastRampGeometry = new THREE.BoxGeometry(14.14, 1, 20);
+    const eastRamp = new THREE.Mesh(eastRampGeometry, rampMaterial);
+    eastRamp.position.set(17, 6, 0);
+    eastRamp.rotation.z = -Math.PI / 4;
+    scene.add(eastRamp);
+    objects.push(eastRamp);
+
+    // West ramp
+    const westRamp = new THREE.Mesh(eastRampGeometry, rampMaterial);
+    westRamp.position.set(-17, 6, 0);
+    westRamp.rotation.z = Math.PI / 4;
+    scene.add(westRamp);
+    objects.push(westRamp);
+
+    // Add corner blocks at the top
+    const blockGeometry = new THREE.BoxGeometry(5, 5, 5);
+    const blockMaterial = new THREE.MeshPhongMaterial({ color: 0x505050 });
+
+    // Northeast block
+    const neBlock = new THREE.Mesh(blockGeometry, blockMaterial);
+    neBlock.position.set(17, 13, -17);
+    scene.add(neBlock);
+    objects.push(neBlock);
+
+    // Northwest block
+    const nwBlock = new THREE.Mesh(blockGeometry, blockMaterial);
+    nwBlock.position.set(-17, 13, -17);
+    scene.add(nwBlock);
+    objects.push(nwBlock);
+
+    // Southeast block
+    const seBlock = new THREE.Mesh(blockGeometry, blockMaterial);
+    seBlock.position.set(17, 13, 17);
+    scene.add(seBlock);
+    objects.push(seBlock);
+
+    // Southwest block
+    const swBlock = new THREE.Mesh(blockGeometry, blockMaterial);
+    swBlock.position.set(-17, 13, 17);
+    scene.add(swBlock);
+    objects.push(swBlock);
+
     // Lighting
     const light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75);
     scene.add(light);
